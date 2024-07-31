c7e6e1765988baa2ec970aea124cd16b56cb12a9
farhank-py <farhan.kashif@arbisoft.com>
2024-07-31 12:24:09 +0500

    Added Websockets and setup a basic Chat App
    Configured settings and asgi.py to work with the channels. Modified Url
    routing for the new app. Setup up views, models, consumers and routing
    for the app. Worked on templates and added neccessart js for the socket
    to work
    
    This Chat App was setup to understand the basics and working of Django
    Channels and Web Sockets

--- transcribe/chatApp/views.py
+++ transcribe/chatApp/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from .models import Room, Message
+from django.shortcuts import redirect
+# from django.template.response import TemplateResponse
 
-# Create your views here.
+
+def CreateRoom(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        room = request.POST['room']
+
+        try:
+            get_room = Room.objects.get(room_name=room)
+            # return redirect('room', room_name=room, username=username)
+        except Room.DoesNotExist:
+            new_room = Room(room_name=room)
+            new_room.save()
+        return redirect('room', room_name=room, username=username)
+
+
+    return render(request, 'chatApp/index.html')
+
+
+def MessageView(request, room_name, username):
+    get_room = Room.objects.get(room_name=room_name)
+    get_messages = Message.objects.filter(room=get_room)
+
+    context = {
+        'messages': get_messages,
+        'user': username,
+        'room_name': room_name,
+    }
+
+    return render(request, 'chatApp/message.html', context)
+
+
